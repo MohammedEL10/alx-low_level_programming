e9c363f65e1ac5d73c160967272e61edc293ab3b
MohammedEL10 <mohammedelhathout14@gmail.com>
2023-10-30 15:37:07 -0700

    C - File I/O

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,42 +10,29 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
+	ssize_t file, let, w;
+	char *text;
 
-	ssize_t read_write;
+	text = malloc(letters);
+	if (text == NULL)
+		return (0);
 
-	char *buffer;
+	if (filename == NULL)
+		return (0);
 
-	if (filename)
+	file = open(filename, O_RDONLY);
+
+	if (file == -1)
 	{
+		free(text);
 		return (0);
 	}
-	/* open file*/
-	fd = open(filename,O_RDONLY);
-	if (fd == -1)
-		return(0);
 
-	/* buffer*/
-	buffer = malloc(sizeof (char) * letters);
-	if (buffer == NULL)
-		return(0);
+	let = read(file, text, letters);
 
-	/* read*/
-	pread - read(fd, buffer, letters);
-	if(_read == -1)
-	{
-		free(buffer);
-		close(fd);
-		return(0);
-	}
-	/* write*/
-	_write = write(STDOUT_FILEND,buffer,_read);
-	if (_write == -1)
-	{
-		free(buffer);
-		close(fd);
-		return(0);
-	}
-	close(fd);
-	return(_read);
+	w = write(STDOUT_FILENO, text, let);
+
+	close(file);
+
+	return (w);
 }
