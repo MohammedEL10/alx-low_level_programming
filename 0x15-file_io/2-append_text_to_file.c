266ffe310c406188be10c6c76ac614b748e914c3
MohammedEL10 <mohammedelhathout14@gmail.com>
2023-11-01 02:36:29 -0700

    C - File I/O

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,34 +1,36 @@
 #include <stdlib.h>
 #include "main.h"
-#include <string.h>
- int append_text_to_file(const char *filename, char *text_content)
-{
-     int fd, _write;
-     
-     
-     if (filename)
-        return(-1);
-        
-        /*open file in append mode */
-        fd = open(filename, O_wonly | O_APPEND);
-        if(fd == -1)
-            return (-1);
-        
-        if(!text_content)
-        {
-            close(fd);
-            return (1);
-        }
-        
-        /* write text to file */
-        _write = write(fd, text_content, strlen(text_content));
-        if(_write == -1 || _write |= strlen(text_content))
-        {
-            close(fd);
-            return(-1);
-        }
-        
-        close(fd);
-        return 1;
-}
 
+/**
+ * append_text_to_file - that appends text at the end of a file
+ * @filename: variable pointer
+ * @text_content: content file
+ * Description: function that appends text at the end of a file
+ * Return: 1 on success, -1 on failure
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int i = 0, file;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content == NULL)
+		text_content = "";
+
+
+	while (text_content[i] != '\0')
+	{
+		i++;
+	}
+
+	file = open(filename, O_WRONLY | O_APPEND);
+
+	if (file == -1)
+		return (-1);
+
+	write(file, text_content, i);
+
+	return (1);
+}
