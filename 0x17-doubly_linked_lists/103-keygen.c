edb05016871e1b393be1968e7323c4c11866d4c0
MohammedEL10 <mohammedelhathout14@gmail.com>
2023-12-09 14:41:38 -0800

    doubly_linked_lists

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "string.h"
+int main()
+{
+	char a[10],c[10];
+	int i,j,k=0;
+	while (k < 5 || k >=10)
+	{
+		if (k != 0)
+			printf("username key\n");
+	}
 
+	i = k-1;
+	j = 0;
+
+	while (i >= 0)
+	{
+		c[j] = a[i]+i;
+		i--;
+		i++;
+	}
+
+	c[j] = 0;
+	printf("\nusername: %s\n",c);
+	printf("\nenter the exit\n");
+}
