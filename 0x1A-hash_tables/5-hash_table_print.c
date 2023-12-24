785ba6dbc261bee157c6f840f2f65c97f4f57c8f
MohammedEL10 <mohammedelhathout14@gmail.com>
2023-12-24 03:33:43 -0800

    hash_tables

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,2 +1,33 @@
 #include "hash_tables.h"
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+#include <stdio.h>
+/**
+ *  function that prints a hash table.
+ *  @:key
+ *  @:value
+ */
 void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int idx = 0;
+	hash_node_t *node;
+	char *comma = "";
+
+	if (!ht || !ht->array)
+		return;
+
+	putchar('{');
+	while (idx < ht->size)
+	{
+		node = ((ht->array)[idx]);
+		while (node)
+		{
+			printf("%s'%s': '%s'", comma, node->key, node->value);
+			comma = ", ";
+			node = node->next;
+		}
+		idx++;
+	}
+	puts("}");
+}
